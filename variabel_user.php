5bfc09d751fe7a4a663013e6c5fb341896fe69eb
Stella3005 <mhmmdguntursquarvants@gmail.com>
2025-02-20 12:23:17 +0700

    praktikum 1

---
+++ variabel_user.php
@@ -0,0 +1,12 @@
+<?php
+//mendefinisikan variabel
+$nama = 'Huda';
+$umur = 19;
+$berat = 60.60;
+
+echo 'nama saya adalah :' .$nama;
+echo '<br/>umur : ' . $umur . ' tahun';
+echo '<br/> berat : ' . $berat . 'kg';
+
+echo "<br/>Hello apa $nama apa kabar?";
+?>
