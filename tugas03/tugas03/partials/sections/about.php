4172aad5dea58558824f0b1ecd47829db101310a
Stella3005 <mhmmdguntursquarvants@gmail.com>
2025-03-13 09:43:47 +0700

    praktikum03

---
+++ tugas03/tugas03/partials/sections/about.php
@@ -0,0 +1,18 @@
+<section class="bg-light py-5">
+                <div class="container px-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-xxl-8">
+                            <div class="text-center my-5">
+                                <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                                <p class="lead fw-light mb-4">Halo! nama saya adalah Muhamad Buana Guntur.</p>
+                                <p class="text-muted">Saya adalah seseorang yang penuh semangat dan senang sekali tertawa, dan saya suka sekali untuk belajar hal hal yang baru.</p>
+                                <div class="d-flex justify-content-center fs-2 gap-4">
+                                    <a class="text-gradient" href="https://www.instagram.com/saltypeepss?igsh=MXNhNmFnNmtubXYwaA%3D%3D&utm_source=qr"><i class="bi bi-instagram"></i></a>
+                                    <a class="text-gradient" href="https://www.linkedin.com/in/muhamad-buana-guntur-2b049b344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><i class="bi bi-linkedin"></i></a>
+                                    <a class="text-gradient" href="https://api.whatsapp.com/send?phone=6289614219718&text="><i class="bi bi-whatsapp"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
